46421201b4a8b8ca32261202c5ff1a43c07a2df1
kislyakoff <kislyakoff@tut.by>
2022-05-19 13:41:39 +0300

    Update Greeting controller and main.mustache

--- src/main/java/by/kislyakoff/sweater/GreetingController.java
+++ src/main/java/by/kislyakoff/sweater/GreetingController.java
@@ -2,13 +2,19 @@ package by.kislyakoff.sweater;
 
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import by.kislyakoff.sweater.domain.Message;
+import by.kislyakoff.sweater.repository.MessageRepo;
+
 @Controller
 public class GreetingController {
+	
+	@Autowired
+	private MessageRepo messageRepo; 
 
 	@GetMapping("/greeting")
 	public String greeting(
@@ -21,7 +27,9 @@ public class GreetingController {
 	
 	@GetMapping
 	public String main(Map<String, Object> model) {
-		model.put("some", "Hello, let's Spring!");
+		
+		Iterable<Message> message = messageRepo.findAll();
+		model.put("messages", "Hello, let's Spring!");
 		return "main";
 	}
 }
